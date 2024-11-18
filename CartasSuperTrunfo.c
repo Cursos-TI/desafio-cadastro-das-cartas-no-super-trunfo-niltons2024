5d312161490700750d8e731328a553587ba223c7
niltons2024 <niltonjr2018@outlook.com>
2024-11-18 17:42:25 +0000

    Informacoes Arizona

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -16,6 +16,37 @@ int main() {
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    printf("Desafio-Cadastro-das-Cartas \n");
 
+    char estado;
+    char codigo[2];
+    char nome[10];
+    int populacao;
+    float area;
+    float PIB;
+    int pontosturisticos;
+
+    printf("Digite seu estado: \n");
+    scanf("%c", &estado);
+
+    printf("Digite seu codigo: \n");
+    scanf("%s", &codigo);
+
+    printf("Digite a populacao: \n");
+    scanf("%d", &populacao);
+
+    printf("Digite seu nome \n");
+    scanf("%s", &nome);
+
+    printf("Digite sua area: \n");
+    scanf("%f", &area);
+
+    printf("Digite o PIB: \n");
+    scanf("%f", &PIB);
+
+    printf("Digite quantos pontos turisticos: \n");
+    scanf("%d", &pontosturisticos);
+
+    
     return 0;
 }
