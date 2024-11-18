c7ffe0df7f30e456f9af45214984b9ac5719339f
niltons2024 <niltonjr2018@outlook.com>
2024-11-18 17:51:38 +0000

    Novo Commit

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -17,6 +17,7 @@ int main() {
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
     printf("Desafio-Cadastro-das-Cartas \n");
+    printf("Novo Commit\n");
 
     char estado;
     char codigo[2];
